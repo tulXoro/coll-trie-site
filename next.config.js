3728b240fac16d7c559e99f8e517f550d1017f8d
tulXoro <tulxoro@hotmail.com>
2022-07-30 00:53:24 -0700

    changed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    loader: 'imgix',
+    path: 'https://example.com/myaccount/',
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
