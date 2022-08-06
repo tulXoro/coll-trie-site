cc85c8b18b19dac4011215b25ee528333977af0b
tulXoro <tulxoro@hotmail.com>
2022-08-05 21:14:16 -0700

    removed copyright from footer

--- components/footer.js
+++ components/footer.js
@@ -21,9 +21,6 @@ export default function Footer() {
                     </li>
                 </ul>
             </div>
-            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://colltrie.com/" className="hover:underline">CollectiveTrie™</a>. All Rights Reserved.
-            </span>
         </footer>
     )
 }
