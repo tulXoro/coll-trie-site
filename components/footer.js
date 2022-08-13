fb756b987639870839c6be146907c9af138e763f
Darren Alain Pagdonsolan Manalo <darrenapm1016@gmail.com>
2022-08-12 19:17:12 -0700

    attempted to change footer format into table

--- components/footer.js
+++ components/footer.js
@@ -6,20 +6,40 @@ export default function Footer() {
                     <img src="" className="mr-3 h-8" alt="Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CollTrie</span>
                 </a>
-                <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
-                    <li>
-                        <a href="#" className="mr-4 hover:underline md:mr-6 ">Services</a>
-                    </li>
-                    <li>
-                        <a href="#" className="mr-4 hover:underline md:mr-6">About Us</a>
-                    </li>
-                    <li>
-                        <a href="#" className="mr-4 hover:underline md:mr-6 ">Projects</a>
-                    </li>
-                    <li>
-                        <a href="#" className="hover:underline">Contact</a>
-                    </li>
-                </ul>
+                <table className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400 table-fixed justify-center">
+                    <thead>
+                        <tr>
+                            <td>
+                                <a href="#" className="mr-4 hover:underline md:mr-6 ">Projects</a>
+                            </td>
+                            <td>
+                                <a href="#" className="mr-4 hover:underline md:mr-6">Services</a>
+                            </td>
+                            <td>
+                                <a href="#" className="mr-4 hover:underline md:mr-6 ">Contact</a>
+                            </td>
+                            <td>
+                                <a href="#" className="hover:underline">About Us</a>
+                            </td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <p>Find out about our work.</p>
+                            </td>
+                            <td>
+                                <p>Learn more about our services.</p>
+                            </td>
+                            <td>
+                                <p>Reach out to us for questions/commissions</p>
+                            </td>
+                            <td>
+                                <p>Get to know about our group and our goals.Reach out to us for questions/commissions.</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </footer>
     )
