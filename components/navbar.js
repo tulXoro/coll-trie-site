8418f38da67934222e6f2bd322840ec5d1459b39
Darren Alain Pagdonsolan Manalo <darrenapm1016@gmail.com>
2022-08-12 18:46:50 -0700

    Navbar style update

--- components/navbar.js
+++ components/navbar.js
@@ -12,16 +12,16 @@ export default function Navbar() {
             </div>
             <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                 <div className="text-sm lg:flex-grow">
-                    <a href="./" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                    <a href="./" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 border-r-2 pr-4">
                         Home
                     </a>
-                    <a href="./projects" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                    <a href="./projects" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 border-r-2 pr-4">
                         Projects
                     </a>
-                    <a href="./services" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                    <a href="./services" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 border-r-2 pr-4">
                         Services
                     </a>
-                    <a href="./contact" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                    <a href="./contact" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 border-r-2 pr-4">
                         Contact
                     </a>
                     <a href="./about" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
