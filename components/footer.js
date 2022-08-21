6ee2ab1c0f9423345ebec6388b06584c8c3d9725
tulXoro <tulxoro@hotmail.com>
2022-08-20 21:05:08 -0700

    fixed the table layout

--- components/footer.js
+++ components/footer.js
@@ -7,38 +7,34 @@ export default function Footer() {
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CollTrie</span>
                 </a>
                 <table className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400 table-fixed justify-center">
-                    <thead>
-                        <tr>
-                            <td>
-                                <a href="#" className="mr-4 hover:underline md:mr-6 ">Projects</a>
-                            </td>
-                            <td>
-                                <a href="#" className="mr-4 hover:underline md:mr-6">Services</a>
-                            </td>
-                            <td>
-                                <a href="#" className="mr-4 hover:underline md:mr-6 ">Contact</a>
-                            </td>
-                            <td>
-                                <a href="#" className="hover:underline">About Us</a>
-                            </td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <p>Find out about our work.</p>
-                            </td>
-                            <td>
-                                <p>Learn more about our services.</p>
-                            </td>
-                            <td>
-                                <p>Reach out to us for questions/commissions</p>
-                            </td>
-                            <td>
-                                <p>Get to know about our group and our goals.Reach out to us for questions/commissions.</p>
-                            </td>
-                        </tr>
-                    </tbody>
+                    <tr>
+                        <td>
+                            <a href="#" className="mr-4 hover:underline md:mr-6 ">Projects</a>
+                        </td>
+                        <td>
+                            <a href="#" className="mr-4 hover:underline md:mr-6">Services</a>
+                        </td>
+                        <td>
+                            <a href="#" className="mr-4 hover:underline md:mr-6 ">Contact</a>
+                        </td>
+                        <td>
+                            <a href="#" className="hover:underline">About Us</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>Find out about our work.</p>
+                        </td>
+                        <td>
+                            <p>Learn more about our services.</p>
+                        </td>
+                        <td>
+                            <p>Reach out to us for questions/commissions</p>
+                        </td>
+                        <td>
+                            <p>Get to know about our group and our goals.Reach out to us for questions/commissions.</p>
+                        </td>
+                    </tr>
                 </table>
             </div>
         </footer>
